b801314069c067b067bf71a22787afc9881177dc
tvpower <mario9871962@hotmail.com>
2024-12-02 20:57:29 -0800

    Updated ui

--- MainWindow.h
+++ MainWindow.h
@@ -3,24 +3,30 @@
 
 #include <QMainWindow>
 #include <QPushButton>
+#include <QtNetwork/QTcpSocket>
+
 QT_BEGIN_NAMESPACE
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 private slots:
+    void handleSocketError(QAbstractSocket::SocketError error);
+    void handleServerResponse();
     void chooseEmulator1();
     void chooseEmulator2();
+    void connectToServer();
 private:
     QPushButton *emulatorBtn1;
     QPushButton *emulatorBtn2;
+    QTcpSocket *m_socket;
 };
 #endif // MAINWINDOW_H
